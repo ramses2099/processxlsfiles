21cf88435710e10052134ce602286cb641777fbe
Jose Encarnacion <Jose.Encarnacion@dpworld.com.do>
2025-07-03 16:45:54 -0400

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import pandas
+import pandas as pd
+import os.path
 
 def main():
     print("test")
+    path_files = "C:\\Users\\d6920\\Sources\\Python\\processxlsfiles\\files\\EquipmentPerformanceReport-20250322_0700.xls"
+    df = pd.read_excel(path_files, sheet_name='Data Source')
+    print(df.head)
 
 if __name__== "__main__":
     main()
